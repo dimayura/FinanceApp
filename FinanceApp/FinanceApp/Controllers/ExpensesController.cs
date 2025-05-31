ba1b09211809d24aa15c39331f73049878135b29
D I Mayura <dimayuranaik@gmail.com>
2025-05-31 20:14:02 +0530

    initial commit

---
+++ FinanceApp/FinanceApp/Controllers/ExpensesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FinanceApp.Data;
+using FinanceApp.Models;
+using FinanceApp.Data.Services;
+
+namespace FinanceApp.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        public readonly IExpensesService _expenseService;
+        public ExpensesController(IExpensesService context)
+        {
+            _expenseService = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var expenses = await _expenseService.GetAll();
+            return View(expenses);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+
+                await _expenseService.Add(expense);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        
+        public IActionResult GetChartData()
+        {
+            var data = _expenseService.GetChartData();
+            return Json(data);
+        }
+    }
+}
---
+++ FinanceApp/FinanceApp/Data/Services/IExpensesService.cs
@@ -0,0 +1,11 @@
+﻿using FinanceApp.Models;
+namespace FinanceApp.Data.Services
+{
+    public interface IExpensesService
+    {
+        Task<IEnumerable<Expense>> GetAll();
+        Task Add(Expense expense);
+
+        IQueryable GetChartData();
+    }
+}
